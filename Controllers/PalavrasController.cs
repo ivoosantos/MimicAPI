e3e9bef052944add7fc79d8f0af721ab4ebd23a9
ivoosantos <oliveiradossantosivo@gmail.com>
2020-04-15 18:56:12 -0300

    Criando paginação das palavras.

--- Controllers/PalavrasController.cs
+++ Controllers/PalavrasController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MimicAPI.Database;
+using MimicAPI.Helpers;
 using MimicAPI.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,12 +23,33 @@ namespace MimicAPI.Controllers
         //APP -- /api/palavras?data=2020-04-14
         [Route("")]
         [HttpGet]
-        public ActionResult ObterTodas(DateTime? data)//A data será para armazenar no app, para depois o aplicativo atualizar as palavras novas. 
+        public ActionResult ObterTodas([FromQuery]PalavraUrlQuery query)//A data será para armazenar no app, para depois o aplicativo atualizar as palavras novas. 
         {
             var item = _banco.Palavras.AsQueryable();
-            if (data.HasValue)
+            if (query.Data.HasValue)
             {
-                item = item.Where(a => a.Criado > data.Value || a.Atulizado > data.Value);
+                item = item.Where(a => a.Criado > query.Data.Value || a.Atulizado > query.Data.Value);
+            }
+
+            if (query.PagNumero.HasValue)
+            {
+                var quantidadeTotalRegistros = item.Count();
+
+                item = item.Skip((query.PagNumero.Value - 1) * query.PagRegistro.Value).Take(query.PagRegistro.Value);
+
+                var paginacao = new Paginacao();
+                paginacao.NumeroPagina = query.PagNumero.Value;
+                paginacao.RegistroPorPagina = query.PagNumero.Value;
+                paginacao.TotalRegistro = quantidadeTotalRegistros;
+                paginacao.TotalPaginas = (int)Math.Ceiling((double)quantidadeTotalRegistros / query.PagRegistro.Value);
+
+                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject( paginacao ));
+
+                if(query.PagNumero > paginacao.TotalPaginas)
+                {
+                    return NotFound();
+                }
+
             }
             return Ok(item);
             //return new JsonResult(_banco.Palavras);
